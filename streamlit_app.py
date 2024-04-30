eb4e18ef25f590042766e62b849260c522c8d51e
Um-koo <77236122+Um-koo@users.noreply.github.com>
2024-04-30 13:53:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,11 +55,11 @@ if session:
         ingredients_string = ''
 
         for fruit_chosen in ingredients_list:
-				ingredients_string += fruit_chosen + ' '
-				st.subheader(fruit_chosen + ' Nutrition Information')
-				fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-				fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-               
+			ingredients_string += fruit_chosen + ' '
+			st.subheader(fruit_chosen + ' Nutrition Information')
+			fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+			fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
         my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients)
                             VALUES ('{ingredients_string}')"""
