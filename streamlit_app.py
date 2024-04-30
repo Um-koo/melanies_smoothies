8539d04a457b5bd477c36e576ad91ce7a6343c29
Um-koo <77236122+Um-koo@users.noreply.github.com>
2024-04-30 13:49:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,12 +48,19 @@ if session:
         , [row[0] for row in my_dataframe.fetchall()]  # fetchall()을 사용하여 데이터를 가져옵니다.
         , max_selections=5
         )
-
+    
     if ingredients_list:
         st.write(ingredients_list)
         st.text(ingredients_list)
+        ingredients_string = ''
+
         
-        ingredients_string = ', '.join(ingredients_list)
+            for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                st.subheader(fruit_chosen + ' Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+               
 
         my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients)
                             VALUES ('{ingredients_string}')"""
