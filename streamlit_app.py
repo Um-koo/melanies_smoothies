1cea396f92c40861f46654cf16fbb9a68d543b21
Um-koo <77236122+Um-koo@users.noreply.github.com>
2024-04-30 14:30:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,10 +53,9 @@ if session:
     if ingredients_list:
         st.write(ingredients_list)
         st.text(ingredients_list)
-        ingredients_string = ''
+        ingredients_string = ' '.join(ingredients_list)  # 각 과일 이름 사이에 공백 추가
 
         for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ''
             st.subheader(fruit_chosen + 'Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
